acfc28d995d640a485fd8074353b0f452c121da8
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-29 19:35:15 -0600

    primera version

---
+++ Colas.py
@@ -0,0 +1,18 @@
+#Colas
+
+cola = ["Carolina","Luis","María","Ángel"]
+
+#Agregando elemenetos por el final de la lista
+cola.append("Marco")
+cola.append("Mariana")
+print(cola)
+
+#Eliminando primer elemento de la lista
+n = cola.pop(0)
+print(f"Atendiendo a: {n}")
+n = cola.pop(0)
+print(f"Atendiendo a: {n}")
+
+print(cola)
+
+#Carolina EM
